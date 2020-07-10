31de1aaab610b74a34467e9a06300d8ef9c3b5f9
Dinesh babu <dineshbabu853@gmail.com>
2020-07-10 13:50:36 +0530

    First Commit

---
+++ Project_round/src/com/FarmerPortal/Product.java
@@ -0,0 +1,27 @@
+package com.FarmerPortal;
+
+public class Product {
+	private int productId;
+	private String name;
+	private int Quantity;
+	
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getQuantity() {
+		return Quantity;
+	}
+	public void setQuantity(int quantity) {
+		Quantity = quantity;
+	}
+	
+}
